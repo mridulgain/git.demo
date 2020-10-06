641eb099f6a479e6ec86ecb15d34206540dd1da1
Mridul <mridul@Mriduls-MacBook-Pro.local>
2020-10-06 13:08:01 +0530

    build team

--- avengers.py
+++ avengers.py
@@ -1,4 +1,12 @@
 import sys
 
-if __name__ == '__main__':
+def default():
     print('Nick: welcome to avengers initiative')
+    
+if  __name__ == '__main__':
+    try:
+        avenger = sys.argv[1]
+        default() 
+    except:
+        default()
+    
